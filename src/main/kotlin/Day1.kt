f2cb19149579b18c835a5e3576fcb4f575356f3e
Arch-vile <mikko.ravimo@gmail.com>
2020-12-01 22:03:37 +0200

    Day 1 part 1 complete

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -1,4 +1,25 @@
-fun main(args: Array<String>) {
-    println("hello")
+import java.io.File
 
+fun readFile(fileName: String)  =
+    File(fileName).readLines()
+
+
+
+fun main(args: Array<String>) {
+//    var numbers = listOf(1721, 979, 366, 299, 675, 1456)
+
+    var numbers = readFile("./src/main/resources/day1Input.txt")
+            .map { it.toInt() }
+
+    var pairs = numbers.map { first ->
+        numbers.map { second -> Pair(first, second) }
+    }.flatten()
+
+    var first = pairs.map { Pair(it, it.first + it.second) }
+            .filter { it.second == 2020 }
+            .map { it.first }
+            .map { it.first * it.second }
+            .first()
+
+    println(first)
 }
