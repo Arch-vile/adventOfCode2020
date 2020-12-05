5036d6fb76931828c6914d9022e0ca8696288534
Arch-vile <mikko.ravimo@gmail.com>
2020-12-05 21:22:18 +0200

    Day 5 part 2 complete

--- src/main/kotlin/day5/Day5.kt
+++ src/main/kotlin/day5/Day5.kt
@@ -3,23 +3,29 @@ package day5
 import readFile
 
 fun main(args: Array<String>) {
-    println(readFile("./src/main/resources/day5Input.txt")
-            .map { getRowAndColumn(it) }
-            .map { it.first * 8 + it.second }
-            .maxOrNull())
+    println(
+            readFile("./src/main/resources/day5Input.txt")
+                    .asSequence()
+                    .map { getRowAndColumn(it) }
+                    .map { it.first * 8 + it.second }
+                    .sortedBy { it }
+                    .windowed(2, 1)
+                    .map { Pair(it, it[1] - it[0]) }
+                    .filter { it.second != 1 }
+                    .map { it.first[0] + 1 }
+                    .first()
+    )
 }
 
-fun getRowAndColumn(boardingPass: String): Pair<Int,Int> {
+fun getRowAndColumn(boardingPass: String): Pair<Int, Int> {
     val asBinary = boardingPass
-            .replace('F','0')
-            .replace('B','1')
-            .replace('R','1')
-            .replace('L','0')
+            .replace("""F|L""".toRegex(), "0")
+            .replace("""B|R""".toRegex(), "1")
 
-    val rowBinary = asBinary.substring(0,7)
+    val rowBinary = asBinary.substring(0, 7)
     val columnBinary = asBinary.substring(7)
     return Pair(asInt(rowBinary), asInt(columnBinary))
 }
 
 fun asInt(seating: String) =
-  Integer.parseInt(seating, 2)
+        Integer.parseInt(seating, 2)
