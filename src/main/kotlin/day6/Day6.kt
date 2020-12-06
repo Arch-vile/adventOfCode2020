fde9aa90d52464b2631a1795371b240be0a334c0
Arch-vile <mikko.ravimo@gmail.com>
2020-12-06 11:47:44 +0200

    Day 6 part 2 complete

--- src/main/kotlin/day6/Day6.kt
+++ src/main/kotlin/day6/Day6.kt
@@ -2,20 +2,29 @@ package day6
 
 import java.io.File
 
-
 fun main(args: Array<String>) {
-    println(
-            File("./src/main/resources/day6Input.txt")
-                    .readText()
-                    .split("\n\n")
-                    .map { countDifferentLetters(it) }
-                    .sum())
-}
+    val questionaires = File("./src/main/resources/day6Input.txt")
+            .readText()
+            .split("\n\n")
 
-private fun countDifferentLetters(test: String) =
-        test
-                .replace("""\s""".toRegex(), "")
-                .toCharArray()
-                .toSet()
-                .size
+    // Part 1
+    println(
+            questionaires.map {
+                it.replace("""\s""".toRegex(), "")
+                        .toCharArray()
+                        .toSet()
+                        .size
+            }.sum())
+
+    // Part 2
+    println(
+            questionaires
+                    .map { it.split("\n") }
+                    .map {
+                        it.map { personAnswers -> personAnswers.toCharArray().toSet() }
+                                .reduce { acc, next -> acc.intersect(next) }
+                                .count()
+                    }.sum()
+    )
+}
 
