a430b18cb92a489da74f22060e8b512248accfcd
Arch-vile <mikko.ravimo@gmail.com>
2020-12-12 17:09:23 +0200

    Day 11 part 2 complete

--- src/main/kotlin/day11/Day11.kt
+++ src/main/kotlin/day11/Day11.kt
@@ -2,38 +2,50 @@ package day11
 
 import readFile
 
-fun main(args: Array<String>) {
+fun straightLineOffsets(xOffset: Int, yOffset: Int): Sequence<Pair<Int, Int>> {
+  var x = xOffset
+  var y = yOffset
 
+  return sequence {
+    while (true) {
+      yield(Pair(x, y))
+      x += xOffset
+      y += yOffset
+    }
+  }
+}
+
+fun main(args: Array<String>) {
   val input = readFile("./src/main/resources/day11Input.txt")
   val width = input[0].length
   val height = input.size
 
-  var lobby = input.joinToString ( "" ).toCharArray().toList()
-  var newLobby  = lobby
+  var lobby = input.joinToString("").toCharArray().toList()
+  var newLobby = lobby
 
   do {
     lobby = newLobby
     newLobby = lobby
       .mapIndexed { index, char ->
-        if(char == 'L') {
-          if (allSurroundingsEmpty(index,lobby, width, height)) {
+        if (char == 'L') {
+          if (allSurroundingsEmpty(index, lobby, width, height)) {
             '#'
           } else {
             'L'
           }
-        } else if(char == '#') {
-          if(hasNeighbourgs(index,lobby,width,height)) {
+        } else if (char == '#') {
+          if (hasNeighbourgs(index, lobby, width, height)) {
             'L'
           } else {
             '#'
           }
         } else '.'
       }
-  } while(!areEqual(lobby, newLobby))
+  } while (!areEqual(lobby, newLobby))
 
 
   println(
-  newLobby.filter { it == '#' }.count()
+    newLobby.filter { it == '#' }.count()
   )
 }
 
@@ -41,45 +53,61 @@ fun areEqual(lobby: List<Char>, newLobby: List<Char>) =
   lobby.toCharArray() contentEquals newLobby.toCharArray()
 
 fun hasNeighbourgs(index: Int, lobby: List<Char>, width: Int, height: Int): Boolean {
-  val coordinates: List<Int> = getSurrounds(index, width, height)
-  var count = coordinates
-    .map {
-      lobby[it] == '#'
-    }.filter { it }
-    .count()
-  return count >= 4
+  val lines: List<List<Int>> = getSurrounds(index, width, height)
+  var count =
+    lines.mapNotNull { line ->
+      line
+        .map { lobby[it] }
+        .firstOrNull { it != '.' }
+    }
+      .filter { it == '#' }
+      .count()
+
+  return count >= 5
 }
 
 fun allSurroundingsEmpty(index: Int, lobby: List<Char>, width: Int, height: Int): Boolean {
-  val coordinates: List<Int> = getSurrounds(index, width, height)
+  val lines: List<List<Int>> = getSurrounds(index, width, height)
 
-  for( coord in coordinates) {
-    if(lobby[coord] == '#')
-      return false
+  for (line in lines) {
+    for (coord in line) {
+      if(lobby[coord] == 'L')
+        break
+
+      if (lobby[coord] == '#')
+        return false
+    }
   }
   return true
 }
 
-private fun getSurrounds(index: Int, width: Int, height: Int): List<Int> {
-  val coordinates: List<Int> = listOfNotNull(
-    relativeTo(index, width, height, -1, -1),
-    relativeTo(index, width, height, -1, 0),
-    relativeTo(index, width, height, -1, 1),
-    relativeTo(index, width, height, 0, -1),
-    relativeTo(index, width, height, 0, 1),
-    relativeTo(index, width, height, 1, -1),
-    relativeTo(index, width, height, 1, 0),
-    relativeTo(index, width, height, 1, 1))
-  return coordinates
+private fun getSurrounds(index: Int, width: Int, height: Int): List<List<Int>> {
+  return listOf(
+    straightLineIndexes(index, width, height, -1, -1),
+    straightLineIndexes(index, width, height, 0, -1),
+    straightLineIndexes(index, width, height, 1, -1),
+    straightLineIndexes(index, width, height, -1, 0),
+    straightLineIndexes(index, width, height, 1, 0),
+    straightLineIndexes(index, width, height, -1, 1),
+    straightLineIndexes(index, width, height, 0, 1),
+    straightLineIndexes(index, width, height, 1, 1)
+  )
 }
 
-fun relativeTo(index: Int, width: Int, height: Int, yOffset: Int, xOffset: Int): Int? {
+private fun straightLineIndexes(index: Int, width: Int, height: Int, xOffset: Int, yOffset: Int): List<Int> {
+  return straightLineOffsets(xOffset, yOffset)
+    .map {
+      relativeTo(index, width, height, it.first, it.second)
+    }.takeWhile { it != null }.toList() as List<Int>
+}
+
+fun relativeTo(index: Int, width: Int, height: Int, xOffset: Int, yOffset: Int): Int? {
   var y = index / width
   var x = index - y * width
   y += yOffset
   x += xOffset
 
-  return if(x < 0 || x >= width || y < 0 || y >= height)
+  return if (x < 0 || x >= width || y < 0 || y >= height)
     null
   else
     x + y * width
