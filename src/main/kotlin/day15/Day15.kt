6a8164e52ac8635644927da525576de611789d3a
Arch-vile <mikko.ravimo@gmail.com>
2020-12-17 22:36:32 +0200

    Day 15 part 2 complete

--- src/main/kotlin/day15/Day15.kt
+++ src/main/kotlin/day15/Day15.kt
@@ -1,24 +1,32 @@
 package day15
 
 fun main(args: Array<String>) {
+  val part1Target = 2020L
+  val target = 30000000L
   val input =
     "19,20,14,0,9,1"
       .split(",")
-      .map { it.toInt() }
+      .map { it.toLong() }
       .toMutableList()
 
-  for (i in 5..2019) {
-    val number = input[i]
-    val lastOccurrenceIndex = input.dropLast(1).lastIndexOf(number)
-    if (lastOccurrenceIndex != -1) {
-      input.add(i - lastOccurrenceIndex)
-    } else {
-      input.add(0)
+  val numbersToIndex  = mutableMapOf<Long,Long>()
+  input.dropLast(1).forEachIndexed {index, number ->
+    numbersToIndex[number] = index.toLong()
+  }
+
+  var number = input.takeLast(1)[0]
+  for (i in input.size-1 until target-1) {
+    val lastLocation = numbersToIndex[number]
+    numbersToIndex[number] = i
+    number = if(lastLocation == null) { 0
+    } else { i - lastLocation }
+
+    // Part 1
+    if(i == part1Target-2) {
+     println(number)
     }
   }
 
-  // Part 1
-  println(input[2019]);
-
-
+  // Part 2
+  println(number)
 }
