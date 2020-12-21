c5913366189782f4dbfea71ba03cdb3b6c444bfd
Arch-vile <mikko.ravimo@gmail.com>
2020-12-21 21:24:38 +0200

    Day 18 part 1 complete

---
+++ src/main/kotlin/day18/Day18.kt
@@ -0,0 +1,68 @@
+package day18
+
+import readFile
+
+fun main(args: Array<String>) {
+  val computations = readFile("./src/main/resources/day18Input.txt")
+
+  val sum = computations
+    .map { calculate(it) }
+    .sum()
+
+  println(sum)
+}
+
+fun calculate(originalCalculation: String): Long {
+  var calculation = originalCalculation
+  while (hasParenthesis(calculation)) {
+    calculation = removeDeepestParenthesis(calculation)
+  }
+  return calculateSimple(calculation)
+}
+
+// Input be like "3 + 2 * 4"
+fun calculateSimple(calculation: String): Long {
+  // Will be like 3,+2,*4
+  val parts = split(calculation)
+  return parts
+    .fold(0L) { acc: Long, part: String ->
+      combine(acc, part)
+    }
+}
+
+// Part be like "+3" or "*4"
+fun combine(acc: Long, part: String): Long {
+  val operation = part.toCharArray()[0]
+  return when (operation) {
+    '+' -> acc + part.replace("+", "").toLong()
+    '*' -> acc * part.replace("*", "").toLong()
+    else -> part.toLong()
+  }
+}
+
+// "3 + 2 * 4" Will be like 3,+2,*4
+fun split(calculation: String): List<String> {
+  return calculation
+    .replace(" ", "")
+    .replace("+", "%+")
+    .replace("*", "%*")
+    .split("%")
+}
+
+// Takes in "3 + ( 4 + (3 * 2))" and return "3 + (4 * 6)"
+fun removeDeepestParenthesis(calculation: String): String {
+  val deepest =
+    """\([^()]*\)""".toRegex()
+      .find(calculation)!!.value
+
+  val simpleCalculation = deepest
+    .replace("(", "")
+    .replace(")", "")
+
+  val value = calculateSimple(simpleCalculation)
+  return calculation.replace(deepest, value.toString())
+}
+
+fun hasParenthesis(calculation: String): Boolean {
+  return calculation.contains('(')
+}
